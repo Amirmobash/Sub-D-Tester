d55a3acd3bf9b9051b3441c3b75ccc2550241c5b
Amir Mobasheraghdam <142031484+Amirmobash@users.noreply.github.com>
2026-06-19 10:17:13 -0700

    Remove unused output pins from ausgangspins array
    Removed unused output pins from the array.

--- arduino/kabeltest_mega/kabeltest_mega.ino
+++ arduino/kabeltest_mega/kabeltest_mega.ino
@@ -6,7 +6,6 @@ const unsigned int PAUSE_MS = 25;
 const byte ausgangspins[ADER_ANZAHL] = {
   22, 23, 24, 25, 26,
   27, 28, 29, 30, 31,
-  32, 33, 34, 35, 36,
   37, 38, 39, 40, 41,
   42, 43, 44, 45, 46
 };
